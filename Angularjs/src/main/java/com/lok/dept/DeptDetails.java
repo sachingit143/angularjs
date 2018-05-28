23b37fc12fb0f4e0e624323afe0d6ff63fd9a418
Asus <sk1894kumar@gmail.com>
2018-05-28 16:55:37 +0530

    adding all the ang files

---
+++ Angularjs/src/main/java/com/lok/dept/DeptDetails.java
@@ -0,0 +1,27 @@
+package com.lok.dept;
+
+public class DeptDetails {
+
+	private int deptno;
+	private String empname;
+	private int sal;
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	
+}
